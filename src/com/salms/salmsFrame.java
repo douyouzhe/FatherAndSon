6d6bf96965fe97804d007d8d9e008a77a214b4ca
douyouzhe <douyouzhe@gmail.com>
2017-09-30 22:35:09 -0400

    1

--- src/com/salms/salmsFrame.java
+++ src/com/salms/salmsFrame.java
@@ -1,9 +1,29 @@
 package com.salms;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 
-
-
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class salmsFrame {
-
+	private JFrame frame;
+	public salmsFrame(){
+		frame = new JFrame("Sales & Logistic Management System");
+		frame.getContentPane().setBackground(new Color(170, 188, 120));
+		frame.addComponentListener(new FrameListener());
+		frame.getContentPane().setLayout(new BorderLayout());
+		frame.setBounds(100, 100, 800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run() {
+				new Login();
+			}
+		});
+		
+	}
 }
