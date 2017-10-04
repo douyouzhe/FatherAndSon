6c83615713248c7b535008ebdc8dd565385f2dee
douyouzhe <douyouzhe@gmail.com>
2017-10-03 23:32:35 -0400

    1

--- src/com/salms/salmsFrame.java
+++ src/com/salms/salmsFrame.java
@@ -3,25 +3,56 @@ package com.salms;
 import java.awt.BorderLayout;
 import java.awt.Color;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 public class salmsFrame {
 	private JFrame frame;
+	private JLabel backLabel;
+	private JDesktopPane desktopPane;
+	
 	public salmsFrame(){
 		frame = new JFrame("Sales & Logistic Management System");
-		frame.getContentPane().setBackground(new Color(170, 188, 120));
-		frame.addComponentListener(new FrameListener());
+		//frame.addComponentListener(new FrameListener());
 		frame.getContentPane().setLayout(new BorderLayout());
-		frame.setBounds(100, 100, 800, 600);
+		frame.setBounds(100, 100, 1200, 1000);
+		frame.getContentPane().setBackground(new Color(170, 188, 120));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		backLabel = new JLabel();
+		backLabel.setVerticalAlignment(SwingConstants.TOP);
+		backLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		//updateBackImage();
+		desktopPane = new JDesktopPane();
+		desktopPane.add(backLabel, new Integer(Integer.MIN_VALUE));
+		frame.getContentPane().add(desktopPane);
+		//JTabbedPane navigationPanel = createNavigationPanel();
+		//frame.getContentPane().add(navigationPanel, BorderLayout.NORTH);
+
+		
+		frame.setVisible(true);
+		
+
 		
 	}
 	
+	private JTabbedPane createNavigationPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void updateBackImage() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run() {
-				new Login();
+				new salmsFrame();
 			}
 		});
 		
