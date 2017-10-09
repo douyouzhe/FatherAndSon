7c0baa05e96a79f7f176622dab69b83db3266b80
douyouzhe <douyouzhe@gmail.com>
2017-10-09 02:09:09 -0400

    1

--- src/com/salms/dao/Dao.java
+++ src/com/salms/dao/Dao.java
@@ -1,7 +1,14 @@
 package com.salms.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import model.salmsSupplier;
+
 
 public class Dao {
 	protected static String dbClassName  = " ";
@@ -26,8 +33,21 @@ public class Dao {
 	public static boolean addSupplierInfo(salmsSupplier supplierInfo) {
 		
 		if (supplierInfo==null) return false;
-		return insert(
-				);
+		return insert("insert tb_gysinfo values('" + supplierInfo.getId() + "','"
+				+ supplierInfo.getName() + "','" + supplierInfo.getNickName() + "','"
+				+ supplierInfo.getAddress() + "','" + supplierInfo.getZipCode() + "','"
+				+ supplierInfo.getTel() + "','" + supplierInfo.getFax() + "','"
+				+ supplierInfo.getContactPpl() + "','" + supplierInfo.getContactTel() + "','"
+				+ supplierInfo.geteMail() + "','" + supplierInfo.getBankBranch() + "')");
 		
+	}
+	public static int updateGys(salmsSupplier gysInfo) {
+		return update("update tb_gysinfo set jc='" + gysInfo.getNickName()
+				+ "',address='" + gysInfo.getAddress() + "',bianma='"
+				+ gysInfo.getZipCode() + "',tel='" + gysInfo.getTel()
+				+ "',fax='" + gysInfo.getFax() + "',lian='" + gysInfo.getContactPpl()
+				+ "',ltel='" + gysInfo.getContactTel() + "',mail='"
+				+ gysInfo.geteMail() + "',yh='" + gysInfo.getBankBranch()
+				+ "' where id='" + gysInfo.getId() + "'");
 	}
 }
---
+++ src/model/salmsSupplier.java
@@ -0,0 +1,109 @@
+package model;
+
+public class salmsSupplier {
+    private String id;
+    private String name;
+    private String nickName;
+    private String address;
+    private String zipCode;
+    private String tel;
+	private String fax;
+    private String contactPpl;
+    private String contactTel;
+    private String bankBranch;
+    private String eMail;
+    
+    public salmsSupplier() {
+    	
+    }
+    
+    public salmsSupplier(String id) {
+        this.id = id;
+    }
+    
+    public salmsSupplier(String id, String name, String nickName, String address, String zipCode, String tel, String fax, String contactPpl, String contactTel, String bankBranch, String eMail) {
+        this.id = id;
+        this.name = name;
+        this.nickName = nickName;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.tel = tel;
+        this.fax = fax;
+        this.contactPpl = contactPpl;
+        this.contactTel = contactTel;
+        this.bankBranch = bankBranch;
+        this.eMail = eMail;
+    }
+    
+    
+    public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getFax() {
+		return fax;
+	}
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+	public String getContactPpl() {
+		return contactPpl;
+	}
+	public void setContactPpl(String contactPpl) {
+		this.contactPpl = contactPpl;
+	}
+	public String getContactTel() {
+		return contactTel;
+	}
+	public void setContactTel(String contactTel) {
+		this.contactTel = contactTel;
+	}
+	public String getBankBranch() {
+		return bankBranch;
+	}
+	public void setBankBranch(String bankBranck) {
+		this.bankBranch = bankBranck;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+
+
+
+}
