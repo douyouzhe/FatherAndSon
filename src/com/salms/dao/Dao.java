730a8dcd9dec80368e0cc4b2e05f2e7b09268f3c
douyouzhe <douyouzhe@gmail.com>
2017-10-12 02:01:21 -0400

    1

--- src/com/salms/dao/Dao.java
+++ src/com/salms/dao/Dao.java
@@ -3,6 +3,7 @@ package com.salms.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -14,55 +15,168 @@ import model.salmsItem;
 
 
 public class Dao {
-	protected static String dbClassName  = " ";
-	protected static String dbUrl = " ";
-	protected static String dbUser = " ";
-	protected static String dbPwd = "";
-	
-	protected static String second = null;
-	
-	public static Connection conn= null;
-		static {
-			try {
-				if(conn == null) {
-					Class.forName(dbClassName).newInstance();
-					conn = DriverManager.getConnection(dbUrl,dbUser,dbPwd);
-				}
-				}catch (Exception ee){
-					ee.printStackTrace();
-				}
-			}
 	private Dao() {
 	}
-	// 读取所有客户信息
-//	public static List getKhInfos() {
-//		List list = findForList("select id,khname from tb_khinfo");
-//		return list;
-//	}
-//	// 读取所有供应商信息
-//	public static List getGysInfos() {
-//		List list = findForList("select id,name from tb_gysinfo");
-//		return list;
-//	}
-		
-	public static boolean addSupplierInfo(salmsSupplier supplierInfo) {
-		
-		if (supplierInfo==null) return false;
-		return insert("insert tb_gysinfo values('" + supplierInfo.getId() + "','"
-				+ supplierInfo.getName() + "','" + supplierInfo.getNickName() + "','"
-				+ supplierInfo.getAddress() + "','" + supplierInfo.getZipCode() + "','"
-				+ supplierInfo.getTel() + "','" + supplierInfo.getFax() + "','"
-				+ supplierInfo.getContactPpl() + "','" + supplierInfo.getContactTel() + "','"
-				+ supplierInfo.geteMail() + "','" + supplierInfo.getBankBranch() + "')");
-		
+// get jdbc connected
+	public static Connection getConnection() throws Exception{
+		try {
+			String driver = "com.mysql.jdbc.Driver";
+			String dbUrl = "jdbc:mysql://localhost:3306/dbSALMS";
+			String dbUser = "root";
+			String dbPwd = "douzi0418";
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(dbUrl,dbUser,dbPwd);
+			System.out.println("SALMS db connected!");
+			return conn;
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return null;
+	}	
+// create all tables
+	public static void creatSupplierTable() throws Exception{
+		try {
+			Connection conn = getConnection();
+			PreparedStatement create = conn.prepareStatement(
+					"CREATE TABLE IF NOT EXISTS "
+					+ "tbSupplier"
+					+ "(id int AUTO_INCREMENT primary key NOT NULL,"
+					+ "name varchar(255),"
+					+ "address varchar(255),"
+					+ "zipCode varchar(255),"
+					+ "tel varchar(255),"
+					+ "fax varchar(255),"
+					+ "contactPpl varchar(255),"
+					+ "contactTel varchar(255),"
+					+ "bankBranch varchar(255),"
+					+ "eMail varchar(255)"
+					+ ")");
+			create.executeUpdate();
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+		finally {
+			System.out.println("Table creation complete!");
+		}
 	}
-	public static int updateGys(salmsSupplier supplierInfo) {
-		return update("update tb_gysinfo set jc='" + supplierInfo.getNickName()
-				+ "',address='" + supplierInfo.getAddress() + "',bianma='"
-				+ supplierInfo.getZipCode() + "',tel='" + supplierInfo.getTel()
-				+ "',fax='" + supplierInfo.getFax() + "',lian='" + supplierInfo.getContactPpl()
-				+ "',ltel='" + supplierInfo.getContactTel() + "',mail='"
-				+ supplierInfo.geteMail() + "',yh='" + supplierInfo.getBankBranch()
-				+ "' where id='" + supplierInfo.getId() + "'");
+	public static void creatItemTable() throws Exception{
+		try {
+			Connection conn = getConnection();
+			PreparedStatement create = conn.prepareStatement(
+					"CREATE TABLE IF NOT EXISTS "
+					+ "tbItem"
+					+ "(id int AUTO_INCREMENT primary key NOT NULL,"
+					+ "itemName varchar(255),"
+					+ "itemNickName varchar(255),"
+					+ "itemOrigin varchar(255),"
+					+ "itemUnit varchar(255),"
+					+ "itemSize varchar(255),"
+					+ "itemPackage varchar(255),"
+					+ "batchNo varchar(255),"
+					+ "govId varchar(255),"
+					+ "supplierName varchar(255),"
+					+ "memo varchar(255)"
+					+ ")");
+			create.executeUpdate();
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+		finally {
+			System.out.println("Table creation complete!");
+		}
 	}
+// add instance to tables	
+	public static void addSupplier(salmsSupplier supplierInfo) throws Exception{		
+		try {
+			Connection conn = getConnection();
+			PreparedStatement added = conn.prepareStatement("INSERT INTO tbSupplier "
+					//+ "(first, last) "
+					+ "VALUES ('"+supplierInfo.getId()+"','"
+					+supplierInfo.getName()+"','"
+					+supplierInfo.getNickName()+"','"
+					+supplierInfo.getAddress()+"','"
+					+supplierInfo.getZipCode()+"','"
+					+supplierInfo.getTel()+"','"
+					+supplierInfo.getFax()+"','"
+					+supplierInfo.getContactPpl()+"','"
+					+supplierInfo.getContactTel()+"','"
+					+supplierInfo.getBankBranch()+"','"
+					+supplierInfo.geteMail()+"'"
+					+ ")");
+			added.executeUpdate();
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		finally {
+			System.out.println("Insertion complete!");
+		}
+	}
+	public static void addItem(salmsItem itemInfo) throws Exception{		
+		try {
+			Connection conn = getConnection();
+			PreparedStatement added = conn.prepareStatement("INSERT INTO tbItem "
+					//+ "(cols) "
+					+ "VALUES ('"+itemInfo.getId()+"','"
+					+itemInfo.getItemName()+"','"
+					+itemInfo.getItemNickName()+"','"
+					+itemInfo.getItemOrigin()+"','"
+					+itemInfo.getItemUnit()+"','"
+					+itemInfo.getItemSize()+"','"
+					+itemInfo.getItemPackage()+"','"
+					+itemInfo.getBatchNo()+"','"
+					+itemInfo.getGovId()+"','"
+					+itemInfo.getMemo()+"','"
+					+itemInfo.getSupplierName()+"'"
+					+ ")");
+			added.executeUpdate();
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		finally {
+			System.out.println("Insertion complete!");
+		}
+	}	
+	
+	
+	public static void main(String[] args) throws Exception {
+//		getConnection();
+//		creatSupplierTable();
+//		creatItemTable();
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+		
+//	public static boolean addSupplierInfo(salmsSupplier supplierInfo) {
+//		
+//		if (supplierInfo==null) return false;
+//		return insert("insert tb_gysinfo values('" + supplierInfo.getId() + "','"
+//				+ supplierInfo.getName() + "','" + supplierInfo.getNickName() + "','"
+//				+ supplierInfo.getAddress() + "','" + supplierInfo.getZipCode() + "','"
+//				+ supplierInfo.getTel() + "','" + supplierInfo.getFax() + "','"
+//				+ supplierInfo.getContactPpl() + "','" + supplierInfo.getContactTel() + "','"
+//				+ supplierInfo.geteMail() + "','" + supplierInfo.getBankBranch() + "')");
+//		
+//	}
+//	public static int updateGys(salmsSupplier supplierInfo) {
+//		return update("update tb_gysinfo set jc='" + supplierInfo.getNickName()
+//				+ "',address='" + supplierInfo.getAddress() + "',bianma='"
+//				+ supplierInfo.getZipCode() + "',tel='" + supplierInfo.getTel()
+//				+ "',fax='" + supplierInfo.getFax() + "',lian='" + supplierInfo.getContactPpl()
+//				+ "',ltel='" + supplierInfo.getContactTel() + "',mail='"
+//				+ supplierInfo.geteMail() + "',yh='" + supplierInfo.getBankBranch()
+//				+ "' where id='" + supplierInfo.getId() + "'");
+//	}
 }
